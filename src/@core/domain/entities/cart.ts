88e7475b5f170a8450f8ab7ebd89e99c81de942e
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-09 07:48:17 -0300

    feat: create cart entity class

---
+++ src/@core/domain/entities/cart.ts
@@ -0,0 +1,29 @@
+import { Product } from "./product"
+
+export type CartProps = {
+    products: Product[]
+}
+
+export class Cart {
+    constructor(public props: CartProps) {}
+
+    addProducts(product: Product) {
+        this.props.products.push(product)
+    }
+
+    removeProducts(productId: number) {
+        this.props.products = this.props.products.filter(product => product.id !== productId)
+    }
+
+    clear() {
+        this.props.products = []
+    }
+
+    get total() {
+        return this.props.products!.reduce((acc, product) => acc + product.price, 0)
+    }
+
+    get products() {
+        return this.props.products
+    }
+}
