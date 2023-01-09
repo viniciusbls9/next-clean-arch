f5bde58c5a4cb685dc5666c89ef4083d95b61c66
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-09 07:19:10 -0300

    feat: create ListProductsUseCase container

--- src/@core/infra/container-registry.ts
+++ src/@core/infra/container-registry.ts
@@ -1,10 +1,12 @@
 import { Container } from "inversify";
+import { ListProductsUseCase } from "../aplication/product/list-product.use-case";
 import { ProductHttpGateway } from "./gateways/product-http.gateway";
 import { http } from "./http";
 
 export const Registry = {
     AxiosAdapter: Symbol.for('AxiosAdapter'),
     ProductGateway: Symbol.for('ProductGateway'),
+    ListProductsUseCase: Symbol.for('ListProductsUseCase'),
 }
 
 export const container = new Container()
@@ -13,4 +15,8 @@ container.bind(Registry.AxiosAdapter).toConstantValue(http)
 
 container.bind(Registry.ProductGateway).toDynamicValue((context) => {
     return new ProductHttpGateway(context.container.get(Registry.AxiosAdapter))
+})
+
+container.bind(Registry.ListProductsUseCase).toDynamicValue((context) => {
+    return new ListProductsUseCase(context.container.get(Registry.AxiosAdapter))
 })
