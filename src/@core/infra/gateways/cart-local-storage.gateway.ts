83c851c770a80425d96bea7c7cabcb05b117abb4
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-11 07:18:38 -0300

    feat: create CartLocalStorageGateway

---
+++ src/@core/infra/gateways/cart-local-storage.gateway.ts
@@ -0,0 +1,15 @@
+import { Cart } from "../../domain/entities/cart";
+import { CartGateway } from "../../domain/gateways/cart.gateway";
+
+export class CartLocalStorageGateway implements CartGateway {
+    private readonly CART_KEY = 'cart'
+
+    get(): Cart {
+        const products = JSON.parse(localStorage.getItem(this.CART_KEY) || '[]')
+       return new Cart({ products: products })
+    }
+    save(cart: Cart): void {
+        localStorage.setItem(this.CART_KEY, JSON.stringify(cart.products))
+    }
+
+}
