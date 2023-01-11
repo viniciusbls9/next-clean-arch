edd5b35d1bd5b6ca40de32ebf840e0f00861ee27
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-11 07:40:18 -0300

    feat: add new containers to Cart useCase

--- src/@core/infra/container-registry.ts
+++ src/@core/infra/container-registry.ts
@@ -1,5 +1,8 @@
 import { Container } from "inversify";
+import { AddProductInCartUseCase } from "../aplication/cart/add-product-in-cart.use-case";
+import { ClearCartUseCase } from "../aplication/cart/clear-cart.use-case";
 import { GetCartUseCase } from "../aplication/cart/get-cart.use-case";
+import { RemoveProductFromCartUseCase } from "../aplication/cart/remove-product-from-cart.use-case";
 import { GetProductUseCase } from "../aplication/product/get-product.use-case";
 import { ListProductsUseCase } from "../aplication/product/list-product.use-case";
 import { CartLocalStorageGateway } from "./gateways/cart-local-storage.gateway";
@@ -8,12 +11,17 @@ import { http } from "./http";
 
 export const Registry = {
     AxiosAdapter: Symbol.for('AxiosAdapter'),
+
     ProductGateway: Symbol.for('ProductGateway'),
     CartGateway: Symbol.for('CartGateway'),
 
     ListProductsUseCase: Symbol.for('ListProductsUseCase'),
     GetProductUseCase: Symbol.for('GetProductUseCase'),
+
     GetCartUseCase: Symbol.for('GetCartUseCase'),
+    AddProductInCartUseCase: Symbol.for('AddProductInCartUseCase'),
+    RemoveProductFromCartUseCase: Symbol.for('RemoveProductFromCartUseCase'),
+    ClearCartUseCase: Symbol.for('ClearCartUseCase'),
 }
 
 export const container = new Container()
@@ -36,4 +44,16 @@ container.bind(Registry.GetProductUseCase).toDynamicValue((context) => {
 
 container.bind(Registry.GetCartUseCase).toDynamicValue((context) => {
     return new GetCartUseCase(context.container.get(Registry.CartGateway))
+})
+
+container.bind(Registry.AddProductInCartUseCase).toDynamicValue((context) => {
+    return new AddProductInCartUseCase(context.container.get(Registry.CartGateway))
+})
+
+container.bind(Registry.RemoveProductFromCartUseCase).toDynamicValue((context) => {
+    return new RemoveProductFromCartUseCase(context.container.get(Registry.CartGateway))
+})
+
+container.bind(Registry.ClearCartUseCase).toDynamicValue((context) => {
+    return new ClearCartUseCase(context.container.get(Registry.CartGateway))
 })
