be92b3c7b18f8d6f5413d213d482d239f925e8da
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-11 07:17:41 -0300

    feat: create add Product in Cart use case

--- src/@core/infra/container-registry.ts
+++ src/@core/infra/container-registry.ts
@@ -1,14 +1,19 @@
 import { Container } from "inversify";
+import { GetCartUseCase } from "../aplication/cart/get-cart.use-case";
 import { GetProductUseCase } from "../aplication/product/get-product.use-case";
 import { ListProductsUseCase } from "../aplication/product/list-product.use-case";
+import { CartLocalStorageGateway } from "./gateways/cart-local-storage.gateway";
 import { ProductHttpGateway } from "./gateways/product-http.gateway";
 import { http } from "./http";
 
 export const Registry = {
     AxiosAdapter: Symbol.for('AxiosAdapter'),
     ProductGateway: Symbol.for('ProductGateway'),
+    CartGateway: Symbol.for('CartGateway'),
+
     ListProductsUseCase: Symbol.for('ListProductsUseCase'),
     GetProductUseCase: Symbol.for('GetProductUseCase'),
+    GetCartUseCase: Symbol.for('GetCartUseCase'),
 }
 
 export const container = new Container()
@@ -19,10 +24,16 @@ container.bind(Registry.ProductGateway).toDynamicValue((context) => {
     return new ProductHttpGateway(context.container.get(Registry.AxiosAdapter))
 })
 
+container.bind(Registry.CartGateway).to(CartLocalStorageGateway)
+
 container.bind(Registry.ListProductsUseCase).toDynamicValue((context) => {
     return new ListProductsUseCase(context.container.get(Registry.ProductGateway))
 })
 
 container.bind(Registry.GetProductUseCase).toDynamicValue((context) => {
     return new GetProductUseCase(context.container.get(Registry.ProductGateway))
+})
+
+container.bind(Registry.GetCartUseCase).toDynamicValue((context) => {
+    return new GetCartUseCase(context.container.get(Registry.CartGateway))
 })
