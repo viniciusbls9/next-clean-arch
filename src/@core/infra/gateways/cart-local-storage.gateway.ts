76147aabc0b0740de7f20be017f099475e02d983
Vinicius Benedito <vinicius.silva@viavarejo.com.br>
2023-01-12 11:25:13 -0300

    feat: get products cart

--- src/@core/infra/gateways/cart-local-storage.gateway.ts
+++ src/@core/infra/gateways/cart-local-storage.gateway.ts
@@ -1,5 +1,6 @@
 import { injectable } from "inversify";
 import { Cart } from "../../domain/entities/cart";
+import { Product } from "../../domain/entities/product";
 import { CartGateway } from "../../domain/gateways/cart.gateway";
 
 @injectable()
@@ -8,7 +9,12 @@ export class CartLocalStorageGateway implements CartGateway {
 
     get(): Cart {
         const products = JSON.parse(localStorage.getItem(this.CART_KEY) || '[]')
-       return new Cart({ products: products })
+       return new Cart({ products: products.map((product: Product) => new Product({
+        id: product.id,
+        title: product.title,
+        description: product.description,
+        price: product.price,
+       })) })
     }
     save(cart: Cart): void {
         localStorage.setItem(this.CART_KEY, JSON.stringify(cart.products))
